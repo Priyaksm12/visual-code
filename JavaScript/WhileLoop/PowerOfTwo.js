ed693fb8d65f022e0c3ba36e308ad04bb6c89c8d
Priyaksm12 <priyankaks.priya@gmail.com>
2022-03-28 20:22:35 +0530

    poweroftwo

--- JavaScript/WhileLoop/PowerOfTwo.js
+++ JavaScript/WhileLoop/PowerOfTwo.js
@@ -4,7 +4,8 @@ let number = prompt("Enter The Range : ");
 let endPoint = Math.pow(2, number);
 let result = 1;
 let index = 1;
-while ((index <= endPoint) && (result <= endPoint) && (result < 256)) {
+while ((index <= endPoint) && (result <= endPoint) && (result < 256))
+ {
     result *= 2;
     console.log("2 ^ " + index + " = " + result);
     index++;
